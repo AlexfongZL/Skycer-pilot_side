1922867f276a3089fec635766959218f2e855629
Alex <alexfong100@live.com>
2021-02-15 18:01:11 +0800

    Added the api code for posting pilot's data.

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 import requests
 
+#payload are the data from pilot to the server.
+payload = {"var_1": "111","var_2": "222","var_3": "333","var_4": "444"}
+
+r = requests.post("http://localhost:8000/api/from_pilot", data = payload)
+
+print r.status_code
+print r.text
